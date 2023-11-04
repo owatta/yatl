fbfb6b45e9d8f9c41180729189212b3cf57fe4f7
owatta <you@example.com>
2023-11-04 15:29:33 +0500

    Initial Commit.

---
+++ Makefile
@@ -0,0 +1,3 @@
+yatl:
+	mkdir -p ./bin
+	cc -o ./bin/yatl ./src/yatl.c -lreadline
---
+++ src/yatl.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <readline/readline.h>
+
+int main(int argc, char** argv) {
+  printf("Hello, world\n");
+
+  return 0;
+}
